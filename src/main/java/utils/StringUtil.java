5d4520f5d604bc03a91a537f9ab57ae1f5fc359e
黄为涛 <741494582@qq.com>
2018-01-16 10:29:58 +0800

    first commit

---
+++ src/main/java/utils/StringUtil.java
@@ -0,0 +1,11 @@
+/**
+* @author 黄为涛
+* @time 2018年1月16日 上午10:28:33
+* @projectName JavaUtils
+*
+**/
+package utils;
+
+public class StringUtil {
+
+}
---
+++ src/test/java/utils/StringUtilTest.java
@@ -0,0 +1,11 @@
+/**
+* @author 黄为涛
+* @time 2018年1月16日 上午10:28:50
+* @projectName JavaUtils
+*
+**/
+package utils;
+
+public class StringUtilTest {
+
+}
