5207c0b4d0c8d2536267b99a69bb10877230c562
taoey <741494582@qq.com>
2018-02-25 14:10:44 +0800

    添加XML对应的依赖,建立XML工具类框架

--- src/main/java/util/XMLUtils.java
+++ src/main/java/util/XMLUtils.java
@@ -1,5 +1,37 @@
 package util;
 
 public class XMLUtils {
-
+	
+	/**
+	 * 建立一个xml文件
+	 */
+	public static void  createXML() {
+		
+	}
+	
+	/**
+	 * 获取特定的XML属性
+	 * @param propertyName
+	 * @return
+	 */
+	public static String getProperty(String propertyName) {
+		String key="";
+		return key;
+	}
+	/**
+	 * 修改XML属性和值
+	 * @param propertyName
+	 * @param propertyKey
+	 * @return
+	 */
+	public static String updateProperty(String propertyName , String propertyKey) {
+		return propertyKey;
+	}
+	/**
+	 * 删除特定的XML属性对应的值
+	 * @param propertyName
+	 */
+	public static void deleteProperty(String propertyName) {
+		
+	}
 }
