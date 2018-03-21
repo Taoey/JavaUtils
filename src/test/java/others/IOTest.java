cb0af4685e9f50d5fa5d9104a8134bfa83903c4c
Taoey <741494582@qq.com>
2018-03-21 14:07:44 +0800

    获取类的加载路径

--- src/test/java/others/IOTest.java
+++ src/test/java/others/IOTest.java
@@ -6,12 +6,22 @@
 **/
 package others;
 
+import java.io.File;
+
 import org.junit.Test;
 
 public class IOTest {
 	
+	/**
+	 * File 操作
+	 * 获取类的加载路径
+	 * /home/taoey/mywork/java/workspace/JavaUtils/target/test-classes
+	 */
 	@Test
-	public void watchFileStatus() {
+	public void test0() {
+        File f = new File(this.getClass().getResource("/").getPath());
+        System.out.println();
+
 		
 	}
 
