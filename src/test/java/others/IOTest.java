b7d4fc7b0339384926ecbf170e832cda2aba866a
Taoey <741494582@qq.com>
2018-03-21 16:14:39 +0800

    打开一个gbk文件，并将其保存为utf-8 文件（Linux系统下）

--- src/test/java/others/IOTest.java
+++ src/test/java/others/IOTest.java
@@ -6,7 +6,17 @@
 **/
 package others;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 
 import org.junit.Test;
 
@@ -21,8 +31,53 @@ public class IOTest {
 	public void test0() {
         File f = new File(this.getClass().getResource("/").getPath());
         System.out.println();
-
+	}
+	
+	/**
+	 * 打开一个gbk文件，并将其保存为utf-8 文件（Linux系统下）
+	 * @throws IOException
+	 */
+	@Test 
+	public void test1() throws IOException {
+		//获得文件流
+		FileInputStream fileInputStream = new FileInputStream(new File("/home/taoey/temp/gbkFile.txt"));
+		//获得解码流
+		InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream,"GBK");
+		//获得缓存流(内存中的数据)
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+		String s = "";	
+		//获得输出文件流
+		FileOutputStream fileOutputStream = new FileOutputStream(new File("/home/taoey/temp/utf-8.txt"));
+		//缓存流到文件输出流
+		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, "UTF-8");
+		while ((s = bufferedReader.readLine()) != null) { // 判断最后一行不存在，为空结束循环
+			outputStreamWriter.write(s);// 将要写入文件的内容，写入到新文件
+		}
+		
+		fileInputStream.close();
+		inputStreamReader.close();
+		
+		outputStreamWriter.flush();
+		outputStreamWriter.close();
+		fileOutputStream.flush();
+		fileInputStream.close();
 		
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
